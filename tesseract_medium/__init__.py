ab7783f55eaa908600d33ed8d56dde1fa0869d4a
Immaculate1022 <stacyjohnson1225@icloud.com>
2026-08-28 13:19:07 -0400

    Export tensor layer from package __init__ and bump to v0.3.0

--- tesseract_medium/__init__.py
+++ tesseract_medium/__init__.py
@@ -8,13 +8,33 @@ Modules:
   dynamics     – Möbius-composed quadratic maps
   orientation  – monodromy / orientation-flip tracker
   visualize    – 2D/3D rendering helpers (including layered projections)
+  tensors      – multilinear algebra layer (rotations, Kronecker nesting,
+                 reflections, hierarchical indices, parallel transport)
 """
 
 from .lattice import TesseractLattice, LayeredTesseractMedium, PHI, fibonacci
 from .dynamics import moebius, iterate, mandelbrot_escape, julia_escape
 from .orientation import OrientationTracker
+from .tensors import (
+    so_n_rotation,
+    tesseract_rotation,
+    penteract_rotation,
+    rotate_points,
+    reflection_tensor,
+    mirror_operator,
+    phi_scaled_embedding,
+    recursive_kronecker_nest,
+    hierarchical_index_tensor,
+    parallel_transport_step,
+    orientation_sign,
+    phi_harmonic_angles,
+    kron,
+    tensor_product,
+    contract,
+    einsum,
+)
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 __all__ = [
     "TesseractLattice",
     "LayeredTesseractMedium",
@@ -25,4 +45,21 @@ __all__ = [
     "mandelbrot_escape",
     "julia_escape",
     "OrientationTracker",
+    # tensor layer
+    "so_n_rotation",
+    "tesseract_rotation",
+    "penteract_rotation",
+    "rotate_points",
+    "reflection_tensor",
+    "mirror_operator",
+    "phi_scaled_embedding",
+    "recursive_kronecker_nest",
+    "hierarchical_index_tensor",
+    "parallel_transport_step",
+    "orientation_sign",
+    "phi_harmonic_angles",
+    "kron",
+    "tensor_product",
+    "contract",
+    "einsum",
 ]
